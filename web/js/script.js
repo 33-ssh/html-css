d714128be035dd033e95f987273f0ad8bcfce582
33-ssh <mountainxu33@gmail.com>
2024-08-12 17:07:13 +0800

    0812

--- web/js/script.js
+++ web/js/script.js
@@ -1,8 +1,25 @@
 // 移除背景影片
 // 使用$啟用jquery
-$(function(){
+$(function(){ /* document ready 一個js檔只要執行一次就可以 */
+    // 移除行動裝置的背景影片
     if($(window).width() < 821){
         $('#about video').remove();
     }
+
+    // 交替漢堡按鈕
+    $('.hamburger').click(function(){
+        // .hamburger交替切換 is-active
+        $(this).toggleClass('is-active');
+        // 交替切換 交替切換 .show
+        $('.navigation').toggleClass('show');
+    });
+
+    // 滑動至指定位置
+    $('.menu a').click(function(){
+        let btn =$(this).attr('href');
+        let pos =$(btn).offset();
+        $('html,body').animation({scrollTop});
+    });
+
 }); 
 
