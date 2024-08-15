3257d2cc256af8dfbf2b334f90905478a0f8feaa
33-ssh <mountainxu33@gmail.com>
2024-08-15 16:59:28 +0800

    0815-1

--- web/js/script.js
+++ web/js/script.js
@@ -1,13 +1,13 @@
 // 移除背景影片
 // 使用$啟用jquery
-$(function(){ /* document ready 一個js檔只要執行一次就可以 */
+$(function () { /* document ready 一個js檔只要執行一次就可以 */
     // 移除行動裝置的背景影片
-    if($(window).width() < 821){
+    if ($(window).width() < 821) {
         $('#about video').remove();
     }
 
     // 交替漢堡按鈕
-    $('.hamburger').click(function(){
+    $('.hamburger').click(function () {
         // .hamburger交替切換 is-active
         $(this).toggleClass('is-active');
         // 交替切換 交替切換 .show
@@ -15,11 +15,29 @@ $(function(){ /* document ready 一個js檔只要執行一次就可以 */
     });
 
     // 滑動至指定位置
-    $('.menu a').click(function(){
-        let btn =$(this).attr('href');
-        let pos =$(btn).offset();
-        $('html,body').animation({scrollTop});
+    $('.menu a').click(function () {
+        let btn = $(this).attr('href');
+        let pos = $(btn).offset();
+        $('html,body').animation({ scrollTop: pos.top},1000);
     });
 
-}); 
+    // 滑動至頂
 
+    $('#gotop').click(function(){
+        $('html,body').animate({scrollTop: 0},1000);    
+    });
+
+    // 至頂按鈕淡出淡入
+    $(window).scroll(function(){
+        if($(this).scrollTop() >200) {
+            $('#gotop').stop().fadeTo('fast',1);
+        }else{
+            $('#gotop').stop().fadeOut('fast');
+        }
+    });
+
+});
+// jQuery Smoove
+$('.smoove').smoove({
+    offset:'20%'
+});
